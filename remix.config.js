36cb088433c50fa31dacb444f2c0a2837ec7b7fc
Binod Nepali <nepalibinod9@gmail.com>
2022-12-16 18:59:58 +0100

    fix: tailwindcss and remix types in config file

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  serverBuildTarget: "deno",
-  server: "./server.ts",
+  serverBuildTarget: 'deno',
+
+  server: './server.ts',
   /*
   If live reload causes page to re-render without changes (live reload is too fast),
   increase the dev server broadcast delay.
@@ -9,7 +10,7 @@ module.exports = {
   If live reload seems slow, try to decrease the dev server broadcast delay.
   */
   devServerBroadcastDelay: 300,
-  ignoredRouteFiles: ["**/.*"],
+  ignoredRouteFiles: ['**/.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
